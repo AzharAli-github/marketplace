5b46b1821e4e57c273a5a656bc19342a1318f780
AzharAli-github <azharborithic11@gmail.com>
2024-09-27 16:42:28 +0500

    pages-one

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { HomeSlider } from "../components/Home/HomeSlider";
+import { HomeCollection } from "../components/Home/HomeCollection";
+export const Home = () => {
+    return (
+        <div className="">
+            <HomeSlider/>
+            <HomeCollection/>
+        </div>
+    )
+}
